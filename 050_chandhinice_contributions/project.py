7bd93f4c51f3e431e00776461eac9c53c5e31754
cechandhini <107467730+cechandhini@users.noreply.github.com>
2022-07-24 15:41:58 +0530

    Add files via upload

---
+++ 050_chandhinice_contributions/project.py
@@ -0,0 +1,36 @@
+## The calculator should be able to read 4 numbers and print the addition and multiplication of ONLY THE EVEN numbers using functions.culator.
+a = int(input(" enter number1"))
+b = int(input(" enter number2"))
+c = int(input(" enter number3"))
+d = int(input(" enter number4"))
+list = [ a, b , c, d ]
+for x in list:
+    if x % 2 == 0:
+        print(x) 
+def even(x):
+    return x + x  , x * x
+     
+print(even)
+
+
+ 
+
+
+       
+
+
+
+        
+     
+ 
+        
+
+        
+        
+        
+
+
+
+
+ 
+
